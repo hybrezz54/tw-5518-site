e0ac535347b80305cc2b7234e331210fb693be97
DGatto <dgattoiii@gmail.com>
2016-01-31 19:11:14 -0500

    stuff.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
 jQuery(document).ready(function($) {
-
     window.alert("This site is still under construction. Please check back later for updates.");
-    
-
-
-
 });
 
+function btnFB() {
+	// document.getElementById("twitter-timeline").style.visibility = "hidden";
+		document.getElementById("fb-page").style.visibility = "visible";
 
-// $('#btnClick').on('click', function() {
-//     if ($('#twitter-timeline').css('display') != 'none') {
-//         $('#fb-timeline').html('#fb-timeline').show().siblings('div').hide();
-//     } else if ($('#fb-timeline').css('display') !='none') {
-//         $('#twitter-timeline').show().siblings('div').hide();
-//     }
-// });
+	// document.getElementById("").style.visibility = "hidden";
+}
